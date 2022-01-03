ee77200a3510c807f4b7174fea6828818a6f3310
hikari <ftphikari@protonmail.com>
2022-01-03 21:05:06 +0200

    mv to ooboontoo

--- install.sh
+++ install.sh
@@ -4,44 +4,30 @@
 install_key() {
  key=$(basename "$1")
  read -p "password for ${key}?: " PASSWD
- mkdir -p ~/.ssh/
+ mkdir -p ~/.ssh
  rm -f ~/.ssh/${key}*
  gpg --batch --passphrase "$PASSWD" -o ~/.ssh/${key} -d ${1}.gpg || return
  chmod 600 ~/.ssh/${key}
  ssh-keygen -y -f ~/.ssh/${key} > ~/.ssh/${key}.pub
 }
 
+mkdir -p ~/dev ~/bin
+
+sudo apt install -y curl git-gui entr tcc golang-go clang libllvm11
+
 curl -Lo /tmp/hikari.key.gpg https://github.com/ftphikari/lib/raw/main/hikari.key.gpg
 
 install_key /tmp/hikari.key
 
 export GIT_SSH_COMMAND="ssh -i ~/.ssh/hikari.key -o IdentitiesOnly=yes"
+export GOBIN=~/bin
 
 git clone git@github.com:ftphikari/lib ~/lib
 
 grep 'lib/profile' ~/.bashrc || echo '. ~/lib/profile' >> ~/.bashrc
 
-yay --needed --noconfirm -Syyuu telegram-desktop go rclone tcc clang mold llvm11 sfeed trash-cli entr lagrange
+go install github.com/ftphikari/findssh@v1.0.1
 
-curl -Lo ~/bin/findssh https://github.com/ftphikari/findssh/releases/download/1.0.0/findssh.elf
-
-chmod +x ~/bin/*
-
-mkdir -p ~/.config/
+mkdir -p ~/.config
 ln -fs ~/lib/gf2_config.ini ~/.config/gf2_config.ini
 
-doas sh -c '
-cat >/etc/sway/config.d/wlsunset.conf <<EOF
-# Greater precision than one decimal place serves no purpose
-# (https://xkcd.com/2170/) other than padding the command-line.
-
-exec wlsunset -l 49.4 -L 36.6
-EOF
-
-cat >/etc/sway/config.d/news.conf <<EOF
-bindsym --to-code {
- XF86Launch1 exec news
- Shift+XF86Launch1 exec news-update
-}
-EOF
-'
